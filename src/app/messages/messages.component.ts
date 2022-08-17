cc83982cd18c8e5b08bdc77aecd7d4b3b1ac4763
unknown <klammer.lukas@hotmail.com>
2022-08-17 08:37:23 +0200

    in messages: get user-data from firebase (once)

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../models/user.class';
 import { MessageService } from '../shared/message.service';
+import { first } from 'rxjs/operators';
 
 
 @Component({
@@ -23,6 +24,18 @@ export class MessagesComponent implements OnInit {
       this.channelId = paramMap.get('id');
       this.messageService.getFromFirebase(this.channelId);
     });
+  }
+
+  getUser() {
+    this.firestore
+      .collection('users')
+      .doc('0ktdB0VydBMemqEwcDIv') // the ID from user that we would get
+      .valueChanges( {idField: 'customIdName'} )
+      .pipe(first())
+      .subscribe((user: any)=>{
+        console.log(user);
+        // this.user = new User(user);
+      });
   }
 
 }
